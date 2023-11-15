f81c276714f4757df0b884a6fa14f53bb9c83959
Christian M Zmasek <czmasek@jcvi.org>
2023-11-14 18:00:12 -0800

    in progress

--- bird_analyzer.py
+++ bird_analyzer.py
@@ -12,9 +12,11 @@ class BirdAnalyzer(object):
     def read_ebird_file(file_name):
         source_df = pd.read_csv(file_name, encoding='unicode_escape', dtype={
             'scientific name': str,
+            'English name': str,
             'order': str,
             'family': str
         })
+        return source_df
 
     @staticmethod
     def read_family_to_feature_map_file(file_name):
@@ -24,7 +26,56 @@ class BirdAnalyzer(object):
             'GENERA': str,
             'FEATURE': str
         })
+        return source_df
+
+    @staticmethod
+    def read(ebird_master_table_file_name, ebird_to_feature_map_file_name):
+        ebird_master_table_df = BirdAnalyzer.read_ebird_file(ebird_master_table_file_name)
+        ebird_to_feature_map_df = BirdAnalyzer.read_family_to_feature_map_file(ebird_to_feature_map_file_name)
+
+        order_to_feature = {}
+        family_to_feature = {}
+        genus_to_feature = {}
+
+        for index, row in ebird_to_feature_map_df.iterrows():
+            order = str(row['ORDER'])
+            family = str(row['FAMILY'])
+            genera = str(row['GENERA'])
+            feature = str(row['FEATURE'])
+            if order != 'nan' and feature != 'nan':
+                order_to_feature[order] = feature
+            if family != 'nan' and feature != 'nan':
+                family_to_feature[family] = feature
+            if genera != 'nan' and feature != 'nan':
+                if genera.find(',') > 0:
+                    for g in genera.split(','):
+                        genus_to_feature[g] = feature
+                else:
+                    genus_to_feature[genera] = feature
+
+        species_to_feature = {}
+
+        for index, row in ebird_master_table_df.iterrows():
+            sn = str(row['scientific name'])
+            cn = str(row['English name'])
+            order = str(row['order'])
+            family = str(row['family'])
+
+            if family.find('(') > 0:
+                family = family[0: family.index('(')].strip()
+            genus = sn[0: sn.find(' ')].strip()
+
+            if sn != 'nan':
+                if order != 'nan' and order in order_to_feature:
+                    species_to_feature[sn] = order_to_feature[order]
+                    if family != 'nan' and family in family_to_feature:
+                        species_to_feature[sn] = family_to_feature[family]
+                        if genus in genus_to_feature:
+                            species_to_feature[sn] = genus_to_feature[genus]
+
+        print(species_to_feature)
 
 
 if __name__ == "__main__":
-    pass
+    BirdAnalyzer.read('/Users/czmasek/Dropbox/WORK/JCVI/DL/ebird_oct_2022.csv',
+                      '/Users/czmasek/Dropbox/WORK/JCVI/DL/ebird_to_feature_map.csv')
